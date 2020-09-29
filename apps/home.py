27aaf9b007c6a2fbd61def3c0d505cc60f53eb93
Andre Pereira <andrespp@gmail.com>
2020-09-29 12:05:05 -0300

    Updates home page

--- apps/home.py
+++ apps/home.py
@@ -4,4 +4,18 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
-layout = html.H3("Home")
+layout = dbc.Jumbotron(
+    [
+        html.H3("Painel DW-Bra", className="display-3"),
+        html.Hr(className="my-2"),
+        html.P(
+            "Ferramenta de visualização de dados do Data Warehouse DW-Bra.",
+        ),
+        html.P(dbc.Button("Mais Informações",
+                          color="primary",
+                          href="https://github.com/andrespp/dw-bra",
+                          external_link=True,
+                         )
+               , className="lead"),
+    ]
+)
